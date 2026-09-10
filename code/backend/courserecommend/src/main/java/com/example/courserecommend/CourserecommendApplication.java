e4065f59b37286dcd12a634e91007e8831d6a37a
PoohSorawit <hillingstudio@gmail.com>
2026-09-10 23:30:06 +0700

    init: Add an important file to the project

---
+++ code/backend/courserecommend/src/main/java/com/example/courserecommend/CourserecommendApplication.java
@@ -0,0 +1,13 @@
+package com.example.courserecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourserecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourserecommendApplication.class, args);
+	}
+
+}
---
+++ code/backend/courserecommend/src/test/java/com/example/courserecommend/CourserecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courserecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourserecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
